0bb8727bd3084c955e2f3675de6ad3cd9eecf16b
hansmboron <hansmateusboron@gmail.com>
2022-06-19 18:24:04 -0300

    primeiro commit

---
+++ src/components/Box.vue
@@ -0,0 +1,26 @@
+<template>
+    <div class="box has-text-weight-bold" :style="estilos">
+        <slot></slot>
+    </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+export default defineComponent({
+    name: 'BoxTracker',
+    data() {
+        return {
+            estilos: {
+                backgroundColor: '#FAF0CA',
+            }
+        }
+    },
+})
+
+</script>
+
+<!-- <style scoped>
+.box {
+    background: #FAF0CA;
+}
+</style> -->
